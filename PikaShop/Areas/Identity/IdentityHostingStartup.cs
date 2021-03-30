a890456d00d7dc4ab6e9e0d2d0b0fca4281b52e2
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-03-30 11:34:52 +0700

    UI - Customize Login Page

---
+++ PikaShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PikaShop.Data;
+
+[assembly: HostingStartup(typeof(PikaShop.Areas.Identity.IdentityHostingStartup))]
+namespace PikaShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

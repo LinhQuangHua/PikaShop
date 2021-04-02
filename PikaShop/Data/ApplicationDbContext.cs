8564c82c2b57323bf95301e727045891f0c0f90d
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-02 16:49:41 +0700

    Add migration Update_models

--- PikaShop/Data/ApplicationDbContext.cs
+++ PikaShop/Data/ApplicationDbContext.cs
@@ -18,5 +18,12 @@ namespace PikaShop.Data
         public DbSet<Topic> Topics { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<CommentPost> CommentPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<RatingProduct> RatingProducts { get; set; }
+
     }
 }
--- PikaShop/Data/Migrations/20210402093855_Update_models.Designer.cs
+++ PikaShop/Data/Migrations/20210402093855_Update_models.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PikaShop.Data;
 
 namespace PikaShop.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210402093855_Update_models")]
+    partial class Update_models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

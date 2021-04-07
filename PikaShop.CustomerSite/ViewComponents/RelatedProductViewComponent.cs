a564057951a77c8f370e88a68a389bc611c2132a
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-08 00:38:35 +0700

    Add function Related products

---
+++ PikaShop.CustomerSite/ViewComponents/RelatedProductViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PikaShop.CustomerSite.Services;
+using System.Threading.Tasks;
+
+namespace PikaShop.CustomerSite.ViewComponents
+{
+    public class RelatedProductViewComponent : ViewComponent
+    {
+        private readonly IProductApiClient _productApiClient;
+
+        public RelatedProductViewComponent(IProductApiClient productApiClient)
+        {
+            _productApiClient = productApiClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var product = await _productApiClient.GetProductByCategory(id);
+
+            return View(product);
+        }
+    }
+}

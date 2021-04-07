d55441c3dd0798356cd2cebf88e03c32c2742767
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-07 18:07:44 +0700

    Edit config image url

---
+++ PikaShop.CustomerSite/Services/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PikaShop.CustomerSite
+{
+    public static class Config
+    {
+        public static string ApiUrl = "https://localhost:44317";
+    }
+}

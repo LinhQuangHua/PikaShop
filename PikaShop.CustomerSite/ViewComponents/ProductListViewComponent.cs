62dfa83035ce2bab06cf0353795f48660ba7bbc2
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-10 22:47:35 +0700

    Show All Products in Shop page

---
+++ PikaShop.CustomerSite/ViewComponents/ProductListViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PikaShop.CustomerSite.Services;
+using System.Threading.Tasks;
+
+namespace PikaShop.CustomerSite.ViewComponents
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+        private readonly IProductApiClient _productApiClient;
+
+        public ProductListViewComponent(IProductApiClient productApiClient)
+        {
+            _productApiClient = productApiClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var product = await _productApiClient.GetProducts();
+
+            return View(product);
+        }
+    }
+}

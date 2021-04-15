a7eb10152db189e124df87919813773701e0efc2
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-15 23:27:43 +0700

    Add User api

---
+++ PikaShop.CustomerSite/Services/Users/IUserApiClient.cs
@@ -0,0 +1,12 @@
+﻿using PikaShop.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace PikaShop.CustomerSite.Services
+{
+    public interface IUserApiClient
+    {
+        Task<IList<UserVm>> GetUsers();
+    }
+}
---
+++ PikaShop.CustomerSite/Services/Users/UserApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using PikaShop.Shared;
+
+
+namespace PikaShop.CustomerSite.Services
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly HttpClient _client;
+        public UserApiClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IList<UserVm>> GetUsers()
+        {
+            var response = await _client.GetAsync("https://localhost:44317/api/user");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<IList<UserVm>>();
+        }
+    }
+}

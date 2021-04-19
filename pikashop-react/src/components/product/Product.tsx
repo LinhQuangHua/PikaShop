6d5b8e3b0358dfd950e609a0f136a043aef18a46
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-19 18:14:08 +0700

    Get list Products in React

--- pikashop-react/src/components/product/Product.tsx
+++ pikashop-react/src/components/product/Product.tsx
@@ -1,13 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import IProduct from "../../interface/IProduct";
+import { Table, Button } from 'reactstrap';
 
-const Product = () => {
-  return (
-    <>
-      <h3>List Products</h3>
-      <Link to="/">Return Home</Link>
-    </>
-  )
-};
+export default class Brand extends React.Component {
+  state = {
+    cates: []
+  }
 
-export default Product;
+  componentDidMount() {
+    axios.get(`https://localhost:44317/api/product`)
+      .then(res => {
+        const cates = res.data;
+        this.setState({ cates });
+        console.log(cates);
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <>
+        <h3>List Products</h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Price</th>
+              <th>Height</th>
+              <th>Weight</th>
+              <th>Quantity</th>
+              <th>Description</th>
+              <th>Brand</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          {this.state.cates.map((cates: IProduct) =>
+            <tbody key={cates.id_product}>
+              <tr>
+                <th scope="row">{cates.id_product}</th>
+                <td>{cates.name_product}</td>
+                <td><img src={"https://localhost:44317/user-content/"+cates.ThumbnailImageUrl} style={{ width: 100, height: 100 }}></img></td>
+                <td>{cates.price}</td>
+                <td>{cates.height}</td>
+                <td>{cates.weight}</td>
+                <td>{cates.quantity}</td>
+                <td>{cates.description}</td>
+                <td>{cates.name_brand}</td>
+                <td>{cates.name_category}</td>
+              </tr>
+            </tbody>)}
+        </Table>
+        <Button color="warning"><Link to="/">Return Home</Link></Button>
+      </>
+
+    )
+  }
+}
---
+++ pikashop-react/src/interface/IProduct.ts
@@ -0,0 +1,14 @@
+export default interface IProduct{
+    id_product:number
+    name_product:string
+    ThumbnailImageUrl:string
+    price:number
+    height:number
+    weight: number
+    quantity:number
+    description:string
+    id_category: number
+    id_brand: number
+    name_category: string
+    name_brand: string
+  }

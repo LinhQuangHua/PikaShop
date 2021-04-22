f0fdde3d7db4e89f84dddcaf10f96d6d10b2eef2
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-22 18:06:28 +0700

    Admin - Post Category

--- pikashop-react/src/components/category/PostCategory.tsx
+++ pikashop-react/src/components/category/PostCategory.tsx
@@ -1,45 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { useSelector } from "react-redux";
+import { selectUser } from "../../store/auth-slice";
 
-export default class PostCategory extends React.Component {
-    state = {
-        name_cate: ''
+export default () => {
+    const [nameCate , setName] = useState('');
+
+    const user = useSelector(selectUser);
+
+
+    const handleChange = (event: any) => {
+        setName(event.target.value);
     }
 
-    handleChange = (event: any) => {
-        this.setState({ name_cate: event.target.value });
-    }
-
-    handleSubmit = (event: any) => {
+    const handleSubmit = (event: any) => {
         event.preventDefault();
-
-        const brand = {
-            name_cate: this.state.name_cate
-        };
-
-        axios.post(`https://localhost:44317/api/category`, { brand })
-            .then(res => {
-                console.log(res);
+        
+        axios.post(
+                `https://pikashop.azurewebsites.net/api/category`, 
+                { name_category: nameCate }, 
+                { headers: {"Authorization" : `Bearer ${user?.token}`},
+            }).then(res => {
                 console.log(res.data);
+            }).catch(err => {
+                console.log(err);
             })
     }
 
-    render() {
         return (
             <>
-            <br/>
-            <h5>Add new category</h5>
-            <Form inline onSubmit={this.handleSubmit}>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="exampleEmail" className="mr-sm-2">Name</Label>
-                    <Input type="text" name="name_brand" placeholder="New category..." onChange={this.handleChange}/>
-                </FormGroup>
-                <Button type="submit">Add</Button>
-            </Form>
-            <hr/>
+                <br/>
+                <h5>Add new category</h5>
+                <Form inline onSubmit={handleSubmit}>
+                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                        <Label for="exampleEmail" className="mr-sm-2">Name</Label>
+                        <Input type="text" value={nameCate} placeholder="New category..." onChange={handleChange}/>
+                    </FormGroup>
+                    <Button type="submit">Add</Button>
+                </Form>
+                <hr/>
             </>
         )
-    }
 }
 

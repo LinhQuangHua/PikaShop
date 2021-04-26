3383bbc99c2fdc20c023ba615415c25cae35cdac
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-04-26 22:43:54 +0700

    Admin - Update Get List Categories

--- pikashop-react/src/components/category/Category.tsx
+++ pikashop-react/src/components/category/Category.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+// import axios from 'axios';
 import ICategory from "../../interface/ICategory";
 import { Table, Button } from 'reactstrap';
 import PostCategory from "./PostCategory";
 import Delete from "../../services/Delete"
-import { hostURL } from "../../config";;
+import GetList from "../../services/GetList";
+// import { hostURL } from "../../config";;
 
 export default function Category(props: any) {
 
@@ -13,13 +14,21 @@ export default function Category(props: any) {
   const [itemSelected, setSelected] = React.useState(null);
 
   const _fetchCategoryData = () => {
-    axios.get(hostURL + `/api/category`)
+    // axios.get(hostURL + `/api/category`)
+    //   .then(res => {
+    //     const cates = res.data;
+    //     setCates(cates);
+    //     console.log(cates);
+    //   })
+    //   .catch(error => console.log(error));
+    GetList("category")
       .then(res => {
         const cates = res.data;
-        setCates(cates);
+        setCates(cates)
         console.log(cates);
+      }).catch(err => {
+        console.log(err);
       })
-      .catch(error => console.log(error));
   }
 
   React.useEffect(() => {

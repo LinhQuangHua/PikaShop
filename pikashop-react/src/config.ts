9e28b2ce7a8d8f4fcd07d40d170721bf913efa16
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-05-04 15:48:35 +0700

    Deploy web on Azure

--- pikashop-react/src/config.ts
+++ pikashop-react/src/config.ts
@@ -1,2 +1,4 @@
-export const hostURL = "https://localhost:44317";
-export const authURL = "http://localhost:3000";
+// export const hostURL = "https://localhost:44317";
+// export const authURL = "http://localhost:3000";
+export const hostURL = "https://backend-linhquanghua.azurewebsites.net";
+export const authURL = "https://salinhquanghua.z23.web.core.windows.net";

629bee19a0e49e72d237d8d1de40ce9334f82992
QUANG HUA LINH <17DH111099@st.huflit.edu.vn>
2021-05-08 19:38:34 +0700

    Return localhost

--- pikashop-react/src/config.ts
+++ pikashop-react/src/config.ts
@@ -1,4 +1,2 @@
-// export const hostURL = "https://localhost:44317";
-// export const authURL = "http://localhost:3000";
-export const hostURL = "https://backend-linhquanghua.azurewebsites.net";
-export const authURL = "https://salinhquanghua.z23.web.core.windows.net";
+export const hostURL = "https://localhost:44317";
+export const authURL = "http://localhost:3000";
